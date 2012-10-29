1d45fa87d7ff93654dd582925c06133deb20de2b
Mike Westerhof <mwesterhof@electric-cloud.com>
2012-10-29 16:21:59 -0500

    Add unplug to the repository

---
+++ Unplug-Plugin/src/project/manifest.pl
@@ -0,0 +1,10 @@
+@files = (
+  ['//project/propertySheet/property[propertyName="v_example0"]/value', 'example0.txt'  ],
+  ['//project/propertySheet/property[propertyName="v_example1"]/value', 'example1.xhtml'],
+  ['//project/propertySheet/property[propertyName="v_example2"]/value', 'example2.xhtml'],
+  ['//project/propertySheet/property[propertyName="v_example3"]/value', 'example3.pl'   ],
+  ['//project/propertySheet/property[propertyName="v_example4"]/value', 'example4.pl'   ],
+  ['//project/propertySheet/property[propertyName="v_example5"]/value', 'example5.pl'   ],
+  ['//project/propertySheet/property[propertyName="v_example6"]/value', 'example6.pl'   ],
+  ['//project/propertySheet/property[propertyName="ec_setup"]/value',   'ec_setup.pl'   ],
+);
