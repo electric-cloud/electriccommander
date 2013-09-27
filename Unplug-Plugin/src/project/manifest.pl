859855b83171028bc162071be68182f83c014bf2
Mike Westerhof <mwesterhof@electric-cloud.com>
2013-09-27 07:18:46 -0500

    Reorganize file naming for the examples - source code only change. Add support for an addition view, showing 8 subtabs, each of which shows one of the examples. Shifted the numbering of the example properties for consistency.

--- Unplug-Plugin/src/project/manifest.pl
+++ Unplug-Plugin/src/project/manifest.pl
@@ -1,10 +1,14 @@
 @files = (
-  ['//project/propertySheet/property[propertyName="v_example0"]/value', 'example0.txt'  ],
-  ['//project/propertySheet/property[propertyName="v_example1"]/value', 'example1.xhtml'],
-  ['//project/propertySheet/property[propertyName="v_example2"]/value', 'example2.xhtml'],
-  ['//project/propertySheet/property[propertyName="v_example3"]/value', 'example3.pl'   ],
-  ['//project/propertySheet/property[propertyName="v_example4"]/value', 'example4.pl'   ],
-  ['//project/propertySheet/property[propertyName="v_example5"]/value', 'example5.pl'   ],
-  ['//project/propertySheet/property[propertyName="v_example6"]/value', 'example6.pl'   ],
-  ['//project/propertySheet/property[propertyName="ec_setup"]/value',   'ec_setup.pl'   ],
+  ['//project/propertySheet/property[propertyName="v_example0"]/value', 'empty.txt'        ],
+  ['//project/propertySheet/property[propertyName="v_example1"]/value', 'simple.txt'       ],
+  ['//project/propertySheet/property[propertyName="v_example2"]/value', 'buildstatus.xhtml'],
+  ['//project/propertySheet/property[propertyName="v_example3"]/value', 'news.xhtml'       ],
+  ['//project/propertySheet/property[propertyName="v_example4"]/value', 'luckynumber.pl'   ],
+  ['//project/propertySheet/property[propertyName="v_example5"]/value', 'jobgrid.pl'       ],
+  ['//project/propertySheet/property[propertyName="v_example6"]/value', 'jobgridrefresh.pl'],
+  ['//project/propertySheet/property[propertyName="v_example7"]/value', 'parameterpanel.pl'],
+  ['//project/propertySheet/property[propertyName="v_example8"]/value', 'empty.txt'        ],
+  ['//project/propertySheet/property[propertyName="v_example9"]/value', 'empty.txt'        ],
+
+  ['//project/propertySheet/property[propertyName="ec_setup"]/value',   'ec_setup.pl'      ],
 );
